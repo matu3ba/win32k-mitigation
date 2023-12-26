f9ba7940bcf09c96d642827594440a6881804bf3
Jan Philipp Hafer <anon_1plus1equals3@mailbox.org>
2023-12-26 23:37:27 +0100

    move child process and standalone test out of tree for experimentation

--- build.zig
+++ build.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const assert = std.debug.assert;
+const mem = std.mem;
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
@@ -37,22 +39,28 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // const lib_unit_tests = b.addTest(.{
-    //     .root_source_file = .{ .path = "src/root.zig" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    //
-    // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const childproc_unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/child_process.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_childproc_unit_tests = b.addRunArtifact(childproc_unit_tests);
+
+    const dep = b.anonymousDependency("test/standalone/child_process", @import("test/standalone/child_process/build.zig"), .{},);
+    const dep_step = dep.builder.default_step;
+    assert(mem.startsWith(u8, dep.builder.dep_prefix, "test."));
+    const dep_prefix_adjusted = dep.builder.dep_prefix["test.".len..];
+    dep_step.name = b.fmt("{s}{s}", .{ dep_prefix_adjusted, dep_step.name });
+
     // const exe_unit_tests = b.addTest(.{
     //     .root_source_file = .{ .path = "src/main.zig" },
     //     .target = target,
     //     .optimize = optimize,
     // });
-    //
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_childproc_unit_tests.step);
+    test_step.dependOn(dep_step);
     // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    //
-    // const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_lib_unit_tests.step);
     // test_step.dependOn(&run_exe_unit_tests.step);
 }
